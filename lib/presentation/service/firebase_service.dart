d13f01c8ff303ef9936bd0aed45bbe50b7481d8e
alvaroo611 <alvaromarmolromero@gmail.com>
2023-11-13 14:46:30 +0100

    SenecaFirebase

---
+++ lib/presentation/service/firebase_service.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+
+class FirebaseService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = kIsWeb 
+    ? GoogleSignIn(clientId: '364563782587-rseg8814nksg9n497rijuj76ulllnt9b.apps.googleusercontent.com',
+                   scopes: ['email']) 
+    : GoogleSignIn(scopes: ['email']);
+
+  Future<String?> signInwithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+      await _auth.signInWithCredential(credential);
+    } on FirebaseAuthException {
+      rethrow;
+    }
+    return null;
+  }
+
+  Future<void> signOutFromGoogle() async{
+    await _googleSignIn.signOut();
+    await _auth.signOut();
+  }
+}
